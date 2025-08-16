fa4ccbc0dd896949a5ffea9725239256a227e4a6
jpfogato <jpfogato@gmail.com>
2025-08-16 16:53:25 +0200

    updated gitignore with relevant Rust context

---
+++ chapter_1/1.3_hello_cargo/target/debug/deps/hello_cargo-755de162e2a3737c.d
@@ -0,0 +1,5 @@
+D:\_repos\others\rust\chapter_1\1.3_hello_cargo\target\debug\deps\hello_cargo-755de162e2a3737c.d: src\main.rs
+
+D:\_repos\others\rust\chapter_1\1.3_hello_cargo\target\debug\deps\libhello_cargo-755de162e2a3737c.rmeta: src\main.rs
+
+src\main.rs:
---
+++ chapter_1/1.3_hello_cargo/target/debug/deps/hello_cargo-96fc1db22b89e05a.d
@@ -0,0 +1,5 @@
+D:\_repos\others\rust\chapter_1\1.3_hello_cargo\target\debug\deps\hello_cargo-96fc1db22b89e05a.d: src\main.rs
+
+D:\_repos\others\rust\chapter_1\1.3_hello_cargo\target\debug\deps\libhello_cargo-96fc1db22b89e05a.rmeta: src\main.rs
+
+src\main.rs:
