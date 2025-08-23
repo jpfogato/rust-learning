078f9316b63b14b75ddf60237b51a2b6c8274edb
jpfogato <jpfogato@gmail.com>
2025-08-23 11:32:51 +0200

    finished 3.2 data types, added 3.3 functions

--- chapter_3/variables/src/main.rs
+++ chapter_3/variables/src/main.rs
@@ -7,14 +7,12 @@ fn main() {
 
         let y = 5;
         let y = y + 1;
-
         {
                 let y = y + 2;
                 println!("The value of y in the inner scope is: {y}");
         }
 
         println!("The original value of y (out of the scope) is: {y}");
-
 
         let z: u32 = 1;
         {
@@ -29,7 +27,50 @@ fn main() {
 
         println!("z's original value is: {z}");
 
-        let a: f32 = 2.0;
-        println!("{a}");
+        // float declaration f32 or f64 (default)
+        let a: f32 = 2.54;
+        println!("float variable a = {a}");
+
+        // tuple binding <-set to mut to allow modification
+        let mut tuple: (i32, f64, u8, &str) = (-22, 6.4, 0xFF, "welcome");
+
+        // destructuring the value of the tuple to the b, c, d and e variables.
+        let (b, c, d, e) = tuple;
+        println!("tuple variable: b = {b}, c = {c}, d = {d} and e = {e}");
+
+        // it is also possible to index the value we want from the tuple and bind them to a new variable
+        let i32_variable = tuple.0;
+        let f64_variable = tuple.1;
+        let u8_variable = tuple.2;
+        let string_variable = tuple.3;
+        println!("{i32_variable}, {f64_variable}, {u8_variable}, {string_variable}");
+
+        // we can modify the elements of the tuple by:
+        tuple.0 = 1;
+        tuple.1 = 3.1416;
+        tuple.2 = 0xAA;
+        tuple.3 = "rust";
+
+        let (b, c, d, e) = tuple;
+        println!("b = {b}, c = {c}, d = {d} and e = {e}");
+
+        // array type declaration
+        let array = [5, 4, 3, 2, 1];
+        let first = array[0];
+        println!("array's first element: {first}");
+
+        // or
+        let array = [0;5]; // initializes an array of 5 elements filled with 0s
+        let first = array[0];
+        println!("array's first element is now: {first}");
+
+        //let first = array[0]; 
+        //let second = array[1];
+
+        // from the quizz:
+        let t = ([1; 2], [3; 4]);
+        let (ta, tb) = t;
+        println!("ta[0] + t1.[0] = {}", ta[0] + t.1[0]);
+        println!("tb = {}", tb[0]);
 
 }
