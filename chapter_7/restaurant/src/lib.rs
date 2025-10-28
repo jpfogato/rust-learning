ad8c479debc8d6cb52303dbc6a938e248e59b9d5
jpfogato <jpfogato@gmail.com>
2025-10-28 19:27:24 +0100

    added lib to understand packaging with modules and submodules from 7.3

---
+++ chapter_7/restaurant/src/lib.rs
@@ -0,0 +1,59 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+        fn seat_at_table() {}
+    }
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+    }
+}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,      // customer can choose bread type
+        seasonal_fruit: String, // but cannot choose seasonal fruit
+    }
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order(); // at the parent (same file, but declared somewhere else)
+    }
+    fn cook_order() {}
+}
+
+fn deliver_order() {}
+
+pub fn eat_at_restaurant() {
+    // absolute path: (preferred method)
+    crate::front_of_house::hosting::add_to_waitlist();
+    // relative path:
+    // front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye"); // public associated function that constructs an instance of Breakfast
+
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal.
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
